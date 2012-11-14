a7e7104b4320f734f77be7cf0995e5c8dc221120
Norman Maurer <nmaurer@redhat.com>
2012-11-14 14:11:02 +0100

    Add code for chapter 1

---
+++ src/main/java/com/manning/nettyinaction/chapter1/PlainNio2EchoServer.java
@@ -0,0 +1,93 @@
+package com.manning.nettyinaction.chapter1;
+
+import java.io.IOException;
+import java.lang.Integer;import java.lang.InterruptedException;import java.lang.Object;import java.lang.Override;import java.lang.String;import java.lang.System;import java.lang.Thread;import java.lang.Throwable;import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author <a href="mailto:nmaurer@redhat.com">Norman Maurer</a>
+ */
+public class PlainNio2EchoServer {
+
+    public void serve(int port) throws IOException {
+        System.out.println("Listening for connections on port " + port);
+        final AsynchronousServerSocketChannel serverChannel = AsynchronousServerSocketChannel.open();
+        InetSocketAddress address = new InetSocketAddress(port);
+        serverChannel.bind(address);
+        final CountDownLatch latch = new CountDownLatch(1);
+        serverChannel.accept(null, new CompletionHandler<AsynchronousSocketChannel, Object>() {
+            @Override
+            public void completed(final AsynchronousSocketChannel channel, Object attachment) {
+                ByteBuffer buffer = ByteBuffer.allocate(100);
+                channel.read(buffer, buffer, new EchoCompletionHandler(channel));
+            }
+
+            @Override
+            public void failed(Throwable throwable, Object attachment) {
+                try {
+                    serverChannel.close();
+                } catch (IOException e) {
+                    // ingnore on close
+                } finally {
+                    latch.countDown();
+                }
+            }
+        });
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+          Thread.currentThread().interrupt();
+        }
+
+    }
+
+    private final class EchoCompletionHandler implements CompletionHandler<Integer, ByteBuffer> {
+        private final AsynchronousSocketChannel channel;
+        EchoCompletionHandler(AsynchronousSocketChannel channel) {
+            this.channel = channel;
+        }
+
+        @Override
+        public void completed(Integer result, ByteBuffer buffer) {
+            buffer.flip();
+            channel.write(buffer, buffer, new CompletionHandler<Integer, ByteBuffer>() {
+                @Override
+                public void completed(Integer result, ByteBuffer buffer) {
+                    if (buffer.hasRemaining()) {
+                        channel.write(buffer, buffer, this);
+                    } else {
+                        buffer.compact();
+                        channel.read(buffer, buffer, EchoCompletionHandler.this);
+                    }
+                }
+
+                @Override
+                public void failed(Throwable exc, ByteBuffer attachment) {
+                    try {
+                        channel.close();
+                    } catch (IOException e) {
+                        // ingnore on close
+                    }
+                }
+            });
+        }
+
+        @Override
+        public void failed(Throwable exc, ByteBuffer attachment) {
+            try {
+                channel.close();
+            } catch (IOException e) {
+                // ingnore on close
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        PlainNio2EchoServer server = new PlainNio2EchoServer();
+        server.serve(8888);
+    }
+}
---
+++ src/main/java/com/manning/nettyinaction/chapter1/PlainNioEchoServer.java
@@ -0,0 +1,74 @@
+package com.manning.nettyinaction.chapter1;
+
+import java.lang.System;import java.net.InetSocketAddress;import java.net.ServerSocket;import java.nio.*;
+import java.nio.ByteBuffer;import java.nio.channels.*;
+import java.net.*;
+import java.nio.channels.SelectionKey;import java.nio.channels.Selector;import java.nio.channels.ServerSocketChannel;import java.nio.channels.SocketChannel;import java.util.*;
+import java.io.IOException;import java.util.Iterator;import java.util.Set;
+
+/**
+ * @author <a href="mailto:nmaurer@redhat.com">Norman Maurer</a>
+ */
+public class PlainNioEchoServer {
+
+    public void serve(int port) throws IOException {
+        System.out.println("Listening for connections on port " + port);
+        ServerSocketChannel serverChannel;
+        Selector selector;
+
+        serverChannel = ServerSocketChannel.open();
+        ServerSocket ss = serverChannel.socket();
+        InetSocketAddress address = new InetSocketAddress(port);
+        ss.bind(address);
+        serverChannel.configureBlocking(false);
+        selector = Selector.open();
+        serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+
+        while (true) {
+
+            try {
+                selector.select();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                // handle in a proper way
+                break;
+            }
+
+            Set readyKeys = selector.selectedKeys();
+            Iterator iterator = readyKeys.iterator();
+            while (iterator.hasNext()) {
+                SelectionKey key = (SelectionKey) iterator.next();
+                iterator.remove();
+                try {
+                    if (key.isAcceptable()) {
+                        ServerSocketChannel server = (ServerSocketChannel) key.channel();
+                        SocketChannel client = server.accept();
+                        System.out.println("Accepted connection from " + client);
+                        client.configureBlocking(false);
+                        client.register(selector, SelectionKey.OP_WRITE | SelectionKey.OP_READ, ByteBuffer.allocate(100));
+                    }
+                    if (key.isReadable()) {
+                        SocketChannel client = (SocketChannel) key.channel();
+                        ByteBuffer output = (ByteBuffer) key.attachment();
+                        client.read(output);
+                    }
+                    if (key.isWritable()) {
+                        SocketChannel client = (SocketChannel) key.channel();
+                        ByteBuffer output = (ByteBuffer) key.attachment();
+                        output.flip();
+                        client.write(output);
+                        output.compact();
+                    }
+                } catch (IOException ex) {
+                    key.cancel();
+                    try {
+                        key.channel().close();
+                    } catch (IOException cex) {
+                    }
+                }
+            }
+        }
+    }
+}
+
