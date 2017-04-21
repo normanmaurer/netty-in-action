4772fe3141b323569d45225c60d5da7e9b2e6be9
hepin1989 <hepin.p@alibaba-inc.com>
2017-04-22 04:22:00 +0800

    + 第13章的中文注释

--- chapter13/src/main/java/nia/chapter13/LogEventEncoder.java
+++ chapter13/src/main/java/nia/chapter13/LogEventEncoder.java
@@ -10,13 +10,14 @@ import java.net.InetSocketAddress;
 import java.util.List;
 
 /**
- * Listing 13.2 LogEventEncoder
+ * 代码清单 13-2 LogEventEncoder
  *
  * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
  */
 public class LogEventEncoder extends MessageToMessageEncoder<LogEvent> {
     private final InetSocketAddress remoteAddress;
 
+    //LogEventEncoder 创建了即将被发送到指定的 InetSocketAddress 的 DatagramPacket 消息
     public LogEventEncoder(InetSocketAddress remoteAddress) {
         this.remoteAddress = remoteAddress;
     }
@@ -28,9 +29,13 @@ public class LogEventEncoder extends MessageToMessageEncoder<LogEvent> {
         byte[] msg = logEvent.getMsg().getBytes(CharsetUtil.UTF_8);
         ByteBuf buf = channelHandlerContext.alloc()
             .buffer(file.length + msg.length + 1);
+        //将文件名写入到 ByteBuf 中
         buf.writeBytes(file);
+        //添加一个 SEPARATOR
         buf.writeByte(LogEvent.SEPARATOR);
+        //将日志消息写入 ByteBuf 中
         buf.writeBytes(msg);
+        //将一个拥有数据和目的地地址的新 DatagramPacket 添加到出站的消息列表中
         out.add(new DatagramPacket(buf, remoteAddress));
     }
 }
