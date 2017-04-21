840ea3a80eb45f8f33714a2aa5d6ac7d6c2d88f7
hepin1989 <hepin.p@alibaba-inc.com>
2017-04-21 20:47:06 +0800

    + 添加第2章的注释

--- chapter2/Server/src/main/java/nia/chapter2/echoserver/EchoServerHandler.java
+++ chapter2/Server/src/main/java/nia/chapter2/echoserver/EchoServerHandler.java
@@ -9,23 +9,27 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
 
 /**
- * Listing 2.1 EchoServerHandler
+ * 代码清单 2-1 EchoServerHandler
  *
  * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
  */
+//标示一个ChannelHandler可以被多个 Channel 安全地共享
 @Sharable
 public class EchoServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         ByteBuf in = (ByteBuf) msg;
+        //将消息记录到控制台
         System.out.println(
                 "Server received: " + in.toString(CharsetUtil.UTF_8));
+        //将接收到的消息写给发送者，而不冲刷出站消息
         ctx.write(in);
     }
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx)
             throws Exception {
+        //将未决消息冲刷到远程节点，并且关闭该 Channel
         ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)
                 .addListener(ChannelFutureListener.CLOSE);
     }
@@ -33,7 +37,9 @@ public class EchoServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx,
         Throwable cause) {
+        //打印异常栈跟踪
         cause.printStackTrace();
+        //关闭该Channel
         ctx.close();
     }
 }
