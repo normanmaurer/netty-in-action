bb0bfc8c488d678267d95148bbcad4fa1d60278e
hepin1989 <hepin.p@alibaba-inc.com>
2017-04-21 22:10:36 +0800

    + 添加第4章的注释

--- chapter4/src/main/java/nia/chapter4/PlainNioServer.java
+++ chapter4/src/main/java/nia/chapter4/PlainNioServer.java
@@ -12,7 +12,7 @@ import java.util.Iterator;
 import java.util.Set;
 
 /**
- * Listing 4.2 Asynchronous networking without Netty
+ * 代码清单 4-2 未使用 Netty 的异步网络编程
  *
  * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
  */
@@ -22,44 +22,54 @@ public class PlainNioServer {
         serverChannel.configureBlocking(false);
         ServerSocket ss = serverChannel.socket();
         InetSocketAddress address = new InetSocketAddress(port);
+        //将服务器绑定到选定的端口
         ss.bind(address);
+        //打开Selector来处理 Channel
         Selector selector = Selector.open();
+        //将ServerSocket注册到Selector以接受连接
         serverChannel.register(selector, SelectionKey.OP_ACCEPT);
         final ByteBuffer msg = ByteBuffer.wrap("Hi!\r\n".getBytes());
         for (;;){
             try {
+                //等待需要处理的新事件；阻塞将一直持续到下一个传入事件
                 selector.select();
             } catch (IOException ex) {
                 ex.printStackTrace();
                 //handle exception
                 break;
             }
+            //获取所有接收事件的SelectionKey实例
             Set<SelectionKey> readyKeys = selector.selectedKeys();
             Iterator<SelectionKey> iterator = readyKeys.iterator();
             while (iterator.hasNext()) {
                 SelectionKey key = iterator.next();
                 iterator.remove();
                 try {
+                    //检查事件是否是一个新的已经就绪可以被接受的连接
                     if (key.isAcceptable()) {
                         ServerSocketChannel server =
                                 (ServerSocketChannel) key.channel();
                         SocketChannel client = server.accept();
                         client.configureBlocking(false);
+                        //接受客户端，并将它注册到选择器
                         client.register(selector, SelectionKey.OP_WRITE |
                                 SelectionKey.OP_READ, msg.duplicate());
                         System.out.println(
                                 "Accepted connection from " + client);
                     }
+                    //检查套接字是否已经准备好写数据
                     if (key.isWritable()) {
                         SocketChannel client =
                                 (SocketChannel) key.channel();
                         ByteBuffer buffer =
                                 (ByteBuffer) key.attachment();
                         while (buffer.hasRemaining()) {
+                            //将数据写到已连接的客户端
                             if (client.write(buffer) == 0) {
                                 break;
                             }
                         }
+                        //关闭连接
                         client.close();
                     }
                 } catch (IOException ex) {
