5630a53148cbf6f1f3bf07ed723311d3f9904c7e
hepin1989 <hepin.p@alibaba-inc.com>
2017-04-21 20:08:08 +0800

    + 添加第1章的注释

--- chapter1/src/main/java/nia/chapter1/ConnectExample.java
+++ chapter1/src/main/java/nia/chapter1/ConnectExample.java
@@ -13,38 +13,43 @@ import java.nio.charset.Charset;
 /**
  * Created by kerr.
  *
- * Listing 1.3 Asynchronous connect
+ * 代码清单 1-3 异步地建立连接
  *
- * Listing 1.4 Callback in action
+ * 代码清单 1-4 回调实战
  */
 public class ConnectExample {
     private static final Channel CHANNEL_FROM_SOMEWHERE = new NioSocketChannel();
 
     /**
-     * Listing 1.3 Asynchronous connect
+     * 代码清单 1-3 异步地建立连接
      *
-     * Listing 1.4 Callback in action
+     * 代码清单 1-4 回调实战
      * */
     public static void connect() {
         Channel channel = CHANNEL_FROM_SOMEWHERE; //reference form somewhere
         // Does not block
+        //异步地连接到远程节点
         ChannelFuture future = channel.connect(
                 new InetSocketAddress("192.168.0.1", 25));
+        //注册一个 ChannelFutureListener，以便在操作完成时获得通知
         future.addListener(new ChannelFutureListener() {
             @Override
             public void operationComplete(ChannelFuture future) {
+                //检查操作的状态
                 if (future.isSuccess()) {
+                    //如果操作是成功的，则创建一个 ByteBuf 以持有数据
                     ByteBuf buffer = Unpooled.copiedBuffer(
                             "Hello", Charset.defaultCharset());
+                    //将数据异步地发送到远程节点。返回一个 ChannelFuture
                     ChannelFuture wf = future.channel()
                             .writeAndFlush(buffer);
                     // ...
                 } else {
+                    //如果发生错误，则访问描述原因的 Throwable
                     Throwable cause = future.cause();
                     cause.printStackTrace();
                 }
             }
         });
-
     }
 }
